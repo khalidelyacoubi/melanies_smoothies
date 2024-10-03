0060e30f8eba426139df7722f8cc05dae6cb43f1
khalidelyacoubi <kha.elyacoubi@gmail.com>
2024-10-03 15:19:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-
-
-# Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!
@@ -14,19 +11,9 @@ st.write(
 )
 
 
-
-# option = st.selectbox(
-#     "What is your favourite fruit?",
-#     ("Banana", "Strawberies", "Peaches"),
-# )
-
-# st.write("You selected:", option)
-
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be ", name_on_order)
 
-
-# session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
@@ -39,28 +26,22 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    # st.write( ingredients_list)
-    # st.text(ingredients_list)
-    
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
-    # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
     time_to_insert = st.button('Submit Order')
-
-    # st.write(my_insert_stmt)
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
          
     
